27b3b8715c8aad2951dd958d65ed9989a4b09abd
unknown <rasto.stankovic@gmail.com>
2020-04-17 14:56:54 +0200

    it should work now on heroku

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const path = require('path');
+//
 var PORT = process.env.PORT || 3000 ;
 const express = require('express');
 const bodyParser = require('body-parser');
+//
+// inports
+const loginRoutes = require('./routes/login');
+const db = require('./util/database');
 
-const db = require('./units/database');
 const app = express();
-
+app.use(bodyParser.urlencoded({extended: false}));
 /*
 db.execute('SELECT * FROM registered_user ')
 .then(result=> {
@@ -15,6 +19,6 @@ db.execute('SELECT * FROM registered_user ')
     console.log(err);
 });
 */
-
+app.use(loginRoutes);
 
 app.listen(PORT);
